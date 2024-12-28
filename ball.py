63045cb2cc082174a37cbd3a8c4affcea4df92eb
Diogo Ferreira <diogo@timefold.ai>
2024-12-28 14:30:25 +0100

    Implement paddle collision with ball with angle

--- ball.py
+++ ball.py
@@ -7,10 +7,11 @@ class Ball:
 
     def __init__(this, screen: pygame.surface):
         this.screen = screen
+        this.max_angle = 75
         this.x = screen.get_width() / 2
         this.y = screen.get_height() / 2
         this.ball_velocity_module = 10
-        this.dx = random.randint(-this.ball_velocity_module, this.ball_velocity_module)
+        this.dx = random.randint(1, this.ball_velocity_module/2)
         this.dy = math.sqrt(this.ball_velocity_module ** 2 - this.dx ** 2)
 
     def is_paddle_colliding(this, paddle: Paddle):
@@ -21,9 +22,18 @@ class Ball:
         this.y += this.dy
         if this.y < 0 or this.y > this.screen.get_height():
             this.dy = -this.dy
-        if this.is_paddle_colliding(paddle1) or this.is_paddle_colliding(paddle2):
+        if this.is_paddle_colliding(paddle1):
+            this.calculate_paddle_hit(paddle1)
+        if this.is_paddle_colliding(paddle2):
+            this.calculate_paddle_hit(paddle2)
             this.dx = -this.dx
-    
 
     def draw(this, screen: pygame.surface):
         pygame.draw.circle(screen, "white", pygame.Vector2(this.x, this.y), 20)
+
+    def calculate_paddle_hit(this, paddle: Paddle):
+        paddle_middle_point = paddle.offset - paddle.height / 2
+        relative_position = 2 * (((this.y - paddle_middle_point) / (paddle.height)) - 1)
+        relative_angle = relative_position * this.max_angle
+        this.dx = this.ball_velocity_module * math.cos(math.radians(relative_angle))
+        this.dy = this.ball_velocity_module * math.sin(math.radians(relative_angle))
