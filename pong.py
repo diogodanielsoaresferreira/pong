9165704749bdbf42800f22be7c3a9712c13c3241
Diogo Ferreira <diogo@timefold.ai>
2024-12-28 19:23:30 +0100

    Add menu with choice for 1 and 2 players

--- pong.py
+++ pong.py
@@ -1,46 +1,55 @@
 import pygame
 from pong_game import PongGame
-
+import pygame_menu
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-running = True
 
-game = PongGame(screen)
-font = pygame.font.Font(None, 80)
+def run_two_players():
+    game = PongGame(screen)
+    font = pygame.font.Font(None, 80)
 
-player_1 = 0
-player_2 = 0
+    player_1 = 0
+    player_2 = 0
 
-while running:
-    screen.fill("black")
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    keys = pygame.key.get_pressed()
+    running = True
+    while running:
+        screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    game.update(keys)
-    player_won = game.is_point_for_player()
+        keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_r]:
-        player_1 = 0
-        player_2 = 0
-        game = PongGame(screen)
+        game.update(keys)
+        player_won = game.is_point_for_player()
 
-    if player_won == 1:
-        player_1 += 1
-        game = PongGame(screen)
-    elif player_won == 2:
-        player_2 += 1
-        game = PongGame(screen)
+        if keys[pygame.K_r]:
+            player_1 = 0
+            player_2 = 0
+            game = PongGame(screen)
 
-    text = font.render(str(player_1) + "-" + str(player_2), 1, (255, 255, 255))
-    screen.blit(text, (screen.get_width() / 2 - 45, 10))
-    game.draw(screen)
-    pygame.display.flip()
+        if player_won == 1:
+            player_1 += 1
+            game = PongGame(screen)
+        elif player_won == 2:
+            player_2 += 1
+            game = PongGame(screen)
 
-    clock.tick(60)
+        text = font.render(str(player_1) + "-" + str(player_2), 1, (255, 255, 255))
+        screen.blit(text, (screen.get_width() / 2 - 45, 10))
+        game.draw(screen)
+        pygame.display.flip()
 
-pygame.quit()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+menu = pygame_menu.Menu('Pong', 400, 300,
+                       theme=pygame_menu.themes.THEME_DARK)
+
+menu.add.button('1-Player', pygame_menu.events.EXIT)
+menu.add.button('2-Player', run_two_players)
+menu.mainloop(screen)
