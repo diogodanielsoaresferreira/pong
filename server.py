53aa1f243d68da127b823e506b6b36febc018338
Diogo Ferreira <diogo@timefold.ai>
2025-01-01 16:44:27 +0100

    Create new game in server and join players to current game

--- server.py
+++ server.py
@@ -1,14 +1,33 @@
 import asyncio
 import json
+import secrets
 from network.events import Events
 
 from websockets.asyncio.server import serve
 
+GAMES = {}
+
 async def start(websocket):
     print("Starting game")
+    game_key = secrets.token_urlsafe(12)
+    GAMES[game_key] = [websocket]
+    print("Created game with key: " + game_key)
 
 async def join(websocket, name):
     print("Joining game: " + name)
+    try:
+        connected = GAMES[name]
+    except KeyError:
+        print("Game not found")
+        #await error(websocket, "Game not found.")
+        return
+
+    if len(connected) >= 2:
+        print("Game already has two players")
+        return
+
+    connected.append(websocket)
+    print("Player added to the game")
 
 async def handler(websocket):
     message = await websocket.recv()
